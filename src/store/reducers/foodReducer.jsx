bab4d40fd25b1a1000daa0a61115829a2cce7274
Daniel (WORK) <perrout@gmail.com>
2019-03-29 15:04:45 -0300

    Dummy data

--- src/store/reducers/foodReducer.jsx
+++ src/store/reducers/foodReducer.jsx
@@ -1,4 +1,554 @@
-const initState = {};
+const initState = {
+  foods: [
+    {
+      id: 1,
+      description: "Arroz, integral, cozido",
+      base_qty: 100,
+      base_unit: "g",
+      category_id: 1,
+      attributes: {
+        humidity: {
+          qty: 70.1386666666667,
+          unit: "percents"
+        },
+        protein: {
+          qty: 2.58825,
+          unit: "g"
+        },
+        lipid: {
+          qty: 1.00033333333333,
+          unit: "g"
+        },
+        cholesterol: {
+          qty: "NA",
+          unit: "mg"
+        },
+        carbohydrate: {
+          qty: 25.80975,
+          unit: "g"
+        },
+        fiber: {
+          qty: 2.74933333333333,
+          unit: "g"
+        },
+        ashes: {
+          qty: 0.463,
+          unit: "g"
+        },
+        calcium: {
+          qty: 5.204,
+          unit: "mg"
+        },
+        magnesium: {
+          qty: 58.702,
+          unit: "mg"
+        },
+        phosphorus: {
+          qty: 105.853,
+          unit: "mg"
+        },
+        iron: {
+          qty: 0.262,
+          unit: "mg"
+        },
+        sodium: {
+          qty: 1.24466666666667,
+          unit: "mg"
+        },
+        potassium: {
+          qty: 75.1516666666667,
+          unit: "mg"
+        },
+        copper: {
+          qty: 0.020333333333333,
+          unit: "mg"
+        },
+        zinc: {
+          qty: 0.682666666666667,
+          unit: "mg"
+        },
+        retinol: {
+          qty: "NA",
+          unit: "mcg"
+        },
+        thiamine: {
+          qty: 0.08,
+          unit: "mg"
+        },
+        riboflavin: {
+          qty: "Tr",
+          unit: "mg"
+        },
+        pyridoxine: {
+          qty: 0.08,
+          unit: "mg"
+        },
+        niacin: {
+          qty: "Tr",
+          unit: "mg"
+        },
+        energy: {
+          kcal: 123.5348925,
+          kj: 516.86999022
+        },
+        fatty_acids: {
+          saturated: {
+            qty: 0.3,
+            unit: "g"
+          },
+          monounsaturated: {
+            qty: 0.4,
+            unit: "g"
+          },
+          polyunsaturated: {
+            qty: 0.3,
+            unit: "g"
+          },
+          "14:0": {
+            qty: "Tr",
+            unit: "g"
+          },
+          "16:0": {
+            qty: 0.25,
+            unit: "g"
+          },
+          "18:0": {
+            qty: 0.02,
+            unit: "g"
+          },
+          "20:0": {
+            qty: "Tr",
+            unit: "g"
+          },
+          "22:0": {
+            qty: "Tr",
+            unit: "g"
+          },
+          "24:0": {
+            qty: "Tr",
+            unit: "g"
+          },
+          "18:1": {
+            qty: 0.36,
+            unit: "g"
+          },
+          "20:1": {
+            qty: "Tr",
+            unit: "g"
+          },
+          "18:2 n-6": {
+            qty: 0.31,
+            unit: "g"
+          },
+          "18:3 n-3": {
+            qty: 0.01,
+            unit: "g"
+          },
+          "22:5": {
+            qty: "Tr",
+            unit: "g"
+          }
+        },
+        manganese: {
+          qty: 0.627333333333333,
+          unit: "mg"
+        }
+      }
+    },
+    {
+      id: 20,
+      description: "Canjica, com leite integral",
+      vitaminC: {
+        qty: "Tr",
+        unit: "mg"
+      },
+      base_qty: 100,
+      base_unit: "g",
+      category_id: 1,
+      attributes: {
+        humidity: {
+          qty: 72.494,
+          unit: "percents"
+        },
+        protein: {
+          qty: 2.36060004234314,
+          unit: "g"
+        },
+        lipid: {
+          qty: 1.24433333333333,
+          unit: "g"
+        },
+        cholesterol: {
+          qty: 1.35066666666667,
+          unit: "mg"
+        },
+        carbohydrate: {
+          qty: 23.6277332909902,
+          unit: "g"
+        },
+        fiber: {
+          qty: 1.21666666666667,
+          unit: "g"
+        },
+        ashes: {
+          qty: 0.273333333333333,
+          unit: "g"
+        },
+        calcium: {
+          qty: 42.5626666666667,
+          unit: "mg"
+        },
+        magnesium: {
+          qty: 5.83833333333333,
+          unit: "mg"
+        },
+        phosphorus: {
+          qty: 40.5833333333333,
+          unit: "mg"
+        },
+        iron: {
+          qty: 0.056,
+          unit: "mg"
+        },
+        sodium: {
+          qty: 27.5866666666667,
+          unit: "mg"
+        },
+        potassium: {
+          qty: 69.809,
+          unit: "mg"
+        },
+        copper: {
+          qty: 0.025666666666667,
+          unit: "mg"
+        },
+        zinc: {
+          qty: 0.278,
+          unit: "mg"
+        },
+        retinol: {
+          qty: "Tr",
+          unit: "mcg"
+        },
+        thiamine: {
+          qty: "Tr",
+          unit: "mg"
+        },
+        riboflavin: {
+          qty: 0.04,
+          unit: "mg"
+        },
+        pyridoxine: {
+          qty: "Tr",
+          unit: "mg"
+        },
+        niacin: {
+          qty: "Tr",
+          unit: "mg"
+        },
+        energy: {
+          kcal: 112.456777220465,
+          kj: 470.519155890427
+        },
+        fatty_acids: {
+          saturated: {
+            qty: 0.603666666666667,
+            unit: "g"
+          },
+          monounsaturated: {
+            qty: 0.365666666666667,
+            unit: "g"
+          },
+          polyunsaturated: {
+            qty: 0.149666666666667,
+            unit: "g"
+          },
+          "12:0": {
+            qty: 0.012333333333333,
+            unit: "g"
+          },
+          "14:0": {
+            qty: 0.083,
+            unit: "g"
+          },
+          "16:0": {
+            qty: 0.374,
+            unit: "g"
+          },
+          "18:0": {
+            qty: 0.146666666666667,
+            unit: "g"
+          },
+          "20:0": {
+            qty: "Tr",
+            unit: "g"
+          },
+          "16:1": {
+            qty: 0.019,
+            unit: "g"
+          },
+          "18:1": {
+            qty: 0.346666666666667,
+            unit: "g"
+          },
+          "18:2 n-6": {
+            qty: 0.149666666666667,
+            unit: "g"
+          },
+          "18:3 n-3": {
+            qty: "Tr",
+            unit: "g"
+          },
+          "18:1t": {
+            qty: 0.024,
+            unit: "g"
+          }
+        },
+        manganese: {
+          qty: 0.02,
+          unit: "mg"
+        }
+      }
+    },
+    {
+      id: 441,
+      description: "Quibe, cru",
+      vitaminC: {
+        qty: "Tr",
+        unit: "mg"
+      },
+      base_qty: 100,
+      base_unit: "g",
+      category_id: 6,
+      attributes: {
+        humidity: {
+          qty: 74.509,
+          unit: "percents"
+        },
+        protein: {
+          qty: 12.3541666666667,
+          unit: "g"
+        },
+        lipid: {
+          qty: 1.66766666666667,
+          unit: "g"
+        },
+        cholesterol: {
+          qty: 26.9343333333333,
+          unit: "mg"
+        },
+        carbohydrate: {
+          qty: 10.7741666666667,
+          unit: "g"
+        },
+        fiber: {
+          qty: 1.64666666666667,
+          unit: "g"
+        },
+        ashes: {
+          qty: 0.695,
+          unit: "g"
+        },
+        calcium: {
+          qty: 12.088,
+          unit: "mg"
+        },
+        magnesium: {
+          qty: 25.6943333333333,
+          unit: "mg"
+        },
+        phosphorus: {
+          qty: 125.79,
+          unit: "mg"
+        },
+        iron: {
+          qty: 1.654,
+          unit: "mg"
+        },
+        sodium: {
+          qty: 38.766,
+          unit: "mg"
+        },
+        potassium: {
+          qty: 241.739666666667,
+          unit: "mg"
+        },
+        copper: {
+          qty: 0.132,
+          unit: "mg"
+        },
+        zinc: {
+          qty: 2.75333333333333,
+          unit: "mg"
+        },
+        retinol: {
+          qty: "Tr",
+          unit: "mcg"
+        },
+        thiamine: {
+          qty: 0.086666666666667,
+          unit: "mg"
+        },
+        riboflavin: {
+          qty: 0.03,
+          unit: "mg"
+        },
+        pyridoxine: {
+          qty: "Tr",
+          unit: "mg"
+        },
+        niacin: {
+          qty: 1.20333333333333,
+          unit: "mg"
+        },
+        energy: {
+          kcal: 109.490669294755,
+          kj: 458.108960329254
+        },
+        fatty_acids: {
+          saturated: {
+            qty: 0.65,
+            unit: "g"
+          },
+          monounsaturated: {
+            qty: 0.696666666666667,
+            unit: "g"
+          },
+          polyunsaturated: {
+            qty: 0.28,
+            unit: "g"
+          },
+          "14:0": {
+            qty: 0.03,
+            unit: "g"
+          },
+          "16:0": {
+            qty: 0.35,
+            unit: "g"
+          },
+          "18:0": {
+            qty: 0.24,
+            unit: "g"
+          },
+          "16:1": {
+            qty: 0.056666666666667,
+            unit: "g"
+          },
+          "18:1": {
+            qty: 0.633333333333333,
+            unit: "g"
+          },
+          "20:1": {
+            qty: 0.006666666666667,
+            unit: "g"
+          },
+          "18:2 n-6": {
+            qty: 0.21,
+            unit: "g"
+          },
+          "18:3 n-3": {
+            qty: 0.03,
+            unit: "g"
+          },
+          "20:4": {
+            qty: 0.02,
+            unit: "g"
+          },
+          "20:5": {
+            qty: 0.01,
+            unit: "g"
+          },
+          "22:5": {
+            qty: 0.01,
+            unit: "g"
+          },
+          "18:1t": {
+            qty: 0.033333333333333,
+            unit: "g"
+          }
+        },
+        amino_acids: {
+          tryptophan: {
+            qty: 0.017,
+            unit: "g"
+          },
+          threonine: {
+            qty: 0.351526,
+            unit: "g"
+          },
+          isoleucine: {
+            qty: 0.3956665,
+            unit: "g"
+          },
+          leucine: {
+            qty: 0.784193,
+            unit: "g"
+          },
+          lysine: {
+            qty: 0.931889,
+            unit: "g"
+          },
+          methionine: {
+            qty: 0.2349774,
+            unit: "g"
+          },
+          cystine: {
+            qty: 0.04195855,
+            unit: "g"
+          },
+          phenylalanine: {
+            qty: 0.4638195,
+            unit: "g"
+          },
+          tyrosine: {
+            qty: 0.453509,
+            unit: "g"
+          },
+          valine: {
+            qty: 0.47736,
+            unit: "g"
+          },
+          arginine: {
+            qty: 0.853145,
+            unit: "g"
+          },
+          histidine: {
+            qty: 0.4987715,
+            unit: "g"
+          },
+          alanine: {
+            qty: 0.6483885,
+            unit: "g"
+          },
+          aspartic: {
+            qty: 0.9982315,
+            unit: "g"
+          },
+          glutamic: {
+            qty: 2.1646525,
+            unit: "g"
+          },
+          glycine: {
+            qty: 0.514505,
+            unit: "g"
+          },
+          proline: {
+            qty: 0.474844,
+            unit: "g"
+          },
+          serine: {
+            qty: 0.4465305,
+            unit: "g"
+          }
+        },
+        manganese: {
+          qty: 0.39,
+          unit: "mg"
+        }
+      }
+    }
+  ]
+};
 
 const foodReducer = (state = initState, action) => {
   return state;
