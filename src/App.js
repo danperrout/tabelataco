f474ab38c24678fae3ca991ef2a5f10f71d670f9
Daniel (WORK) <perrout@gmail.com>
2019-03-29 14:02:40 -0300

    Rota no App Ok.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
+import { BrowserRouter } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Ok</h1>
-      </div>
+      <BrowserRouter>
+        <div>
+          <h1>Ok</h1>
+        </div>
+      </BrowserRouter>
     );
   }
 }
