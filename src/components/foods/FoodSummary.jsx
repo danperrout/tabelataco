bc7795fda016c8c8d94be07a15b4d958c2965fd9
Daniel Perrout (Home) <perrout@gmail.com>
2019-03-31 00:39:01 -0300

    Solução com googleImage

--- src/components/foods/FoodSummary.jsx
+++ src/components/foods/FoodSummary.jsx
@@ -1,20 +1,59 @@
-import React from "react";
+import React, { Component } from "react";
+import GoogleImageSearch from "../../utils/googleSearchImage";
 
-const FoodSummary = ({ food }) => {
-  return (
-    <div className="card z-depth-0 project-summary">
-      <div className="card-content grey-text text-darken-3">
-        <span className="card-title">{food.description}</span>
-        <p>Calorias: {parseFloat(food.energy_kcal).toFixed(2)}kcal por 100g</p>
-        <p>Proteína: {parseFloat(food.protein_g).toFixed(2)}g por 100g</p>
-        <p>Gorduras: {parseFloat(food.lipid_g).toFixed(2)}g por 100g</p>
-        <p>
-          Carboidratos: {parseFloat(food.carbohydrate_g).toFixed(2)}g por 100g
-        </p>
-        <p className="grey-text">{food.category}</p>
+class FoodSummary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      imageEnd:
+        "http://2.bp.blogspot.com/-5DR0FBaig9w/Uc3nFVASYiI/AAAAAAAADr8/kkPyN2yspOw/s232/null-value.gif"
+    };
+  }
+
+  componentDidMount() {
+    // fetch the project name, once it retrieves resolve the promsie and update the state.
+    const imageWeb = GoogleImageSearch.searchImage(
+      this.props.food.description
+    ).then(res => res[0]);
+
+    imageWeb.then(v => {
+      console.log(v);
+      this.setState({
+        ...this.state,
+        imageEnd: v
+      });
+    });
+  }
+
+  render() {
+    const { food } = this.props;
+
+    return (
+      <div className="card z-depth-0 project-summary">
+        <div className="card-image">
+          {this.state.imageEnd && (
+            <img
+              style={{ width: 100, height: 100 }}
+              src={this.state.imageEnd}
+              alt={food.description}
+            />
+          )}
+        </div>
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">{food.description}</span>
+          <p>
+            Calorias: {parseFloat(food.energy_kcal).toFixed(2)}kcal por 100g
+          </p>
+          <p>Proteína: {parseFloat(food.protein_g).toFixed(2)}g por 100g</p>
+          <p>Gorduras: {parseFloat(food.lipid_g).toFixed(2)}g por 100g</p>
+          <p>
+            Carboidratos: {parseFloat(food.carbohydrate_g).toFixed(2)}g por 100g
+          </p>
+          <p className="grey-text">{food.category}</p>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default FoodSummary;
