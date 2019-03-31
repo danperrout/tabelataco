4a00fa5f6ae91ca8ba870c3f52ad2022b2c5a5c3
Daniel Perrout (Home) <perrout@gmail.com>
2019-03-31 10:30:10 -0300

    Paginação base

--- src/components/foods/FoodList.jsx
+++ src/components/foods/FoodList.jsx
@@ -2,13 +2,18 @@ import React from "react";
 import FoodSummary from "./FoodSummary";
 
 const FoodList = ({ foods }) => {
+  // ...inside the render() function
+
+  var size = 10;
+  var items = foods.slice(0, size).map(i => {
+    return <FoodSummary food={i} key={i.id} />;
+  });
+
   return (
     <div className="project-list section">
-      Foram carregados {foods.length} alimentos.
-      {foods &&
-        foods.map(food => {
-          return <FoodSummary food={food} key={food.id} />;
-        })}
+      Foram carregados {foods.length} alimentos. Mostrando {items.length}{" "}
+      alimentos.
+      {items}
     </div>
   );
 };
